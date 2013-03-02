fb69949035443cd8428ad2eb494d6efd6cc66ac5
eiu165 <eiu165@gmail.com>
2013-03-01 21:22:46 -0600

    moved everything to Home/Contact

--- source/Web/Controllers/HomeController.cs
+++ source/Web/Controllers/HomeController.cs
@@ -76,14 +76,6 @@ namespace Web.Controllers
     public class ContactViewModel
     {
         public string Name { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address1 { get; set; }
-        public string Address2 { get; set; }
-        public string Company { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Zip { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public string Comment { get; set; }
